5ce5ea0bf3a6ff6d564e88117b7746a2ffcb7d59
pedro-ca <pedro.henric.rosario@gmail.com>
2025-02-19 14:49:51 -0300

    Adicionando camada model

---
+++ Fornecedores-WebAPI/Fornecedores-Model/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Fornecedores_Model
+{
+    public abstract class BaseEntity
+    {
+        protected int id;
+
+        public int Id { get => id; set => id = value; }
+        public abstract string Validate();
+        public abstract override bool Equals(object obj);
+        public abstract override int GetHashCode();
+    }
+}
---
+++ Fornecedores-WebAPI/Fornecedores-Model/Features/Supplier.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Fornecedores_Model.Features
+{
+    internal class Supplier : BaseEntity
+    {
+        public string Name;
+        public string Email;
+
+        public override string Validate()
+        {
+            var result = "";
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                result += "* Attribute Name cannot be null or white space.\n";
+            }
+            if (string.IsNullOrEmpty(Email))        //todo: real email validation
+            {
+                result += "* Attribute Email cannot be null or white space.\n";
+            }
+            if (result == "")
+                result = "VALID";
+            return result;
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Supplier supplier &&
+                   id == supplier.id &&
+                   Id == supplier.Id &&
+                   Name == supplier.Name &&
+                   Email == supplier.Email;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id, Id, Name, Email);
+        }
+    }
+}
